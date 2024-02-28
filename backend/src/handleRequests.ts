718b6a04f993fa0400e9cefd7c790e2a4f33e39c
apyd <arturpydych@gmail.com>
2024-02-28 21:34:13 +0100

    refactor: updated requests handler

--- backend/src/handleRequests.ts
+++ backend/src/handleRequests.ts
@@ -1,19 +1,20 @@
 import { MESSAGE_TYPE } from "./constants"
-import { onRegistration, onCreateRoom } from "./controller/game.controller"
+import { 
+  handleRegRequest, 
+  handleCreateRoomRequest, 
+  handleAddUserToRoomRequest,
+  // handleAddShipsRequest,
+  // handleAttackRequest,
+  // handleRandomAttackRequest
+} from "./controller/game.controller"
 
-export const processData = (type: string) => {
+export const getRequestHandler = (type: string) => {
   return {
-    [MESSAGE_TYPE.reg]: onRegistration,
-    [MESSAGE_TYPE.create_room]: onCreateRoom,
-    // [MESSAGE_TYPE.add_user_to_room]: onAddUserToRoom,
-    // [MESSAGE_TYPE.add_ships]: onAddShips, 
-    // [MESSAGE_TYPE.attack]: attack, 
-    // [MESSAGE_TYPE.randomAttack]: randomAttack, 
-    // [MESSAGE_TYPE.update_winners]: updateWinners, 
-    // [MESSAGE_TYPE.add_user_to_room]: addUserToRoom, 
-    // [MESSAGE_TYPE.update_room]: updateRoom, 
-    // [MESSAGE_TYPE.start_game]: startGame, 
-    // [MESSAGE_TYPE.turn]: turn, 
-    // [MESSAGE_TYPE.finish]: finish
+    [MESSAGE_TYPE.reg]: handleRegRequest,
+    [MESSAGE_TYPE.create_room]: handleCreateRoomRequest,
+    [MESSAGE_TYPE.add_user_to_room]: handleAddUserToRoomRequest,
+    // [MESSAGE_TYPE.add_ships]: handleAddShipsRequest,
+    // [MESSAGE_TYPE.attack]: handleAttackRequest,
+    // [MESSAGE_TYPE.randomAttack]: handleRandomAttackRequest,
   }[type]
 }
