1fe9c96b7437bce51a7353e89739dec58a5a2da4
apyd <arturpydych@gmail.com>
2024-02-28 21:33:31 +0100

    types: add types to game and player services

--- backend/src/service/game.service.ts
+++ backend/src/service/game.service.ts
@@ -1,10 +1,17 @@
+import crypto from 'node:crypto'
 import db from '../db/db'
+import { AddUserToRoomRequestData, CommandsSequenceInput, CreateRoomRequestData } from '../types'
 
-export const updateRoom = (props: any) => {
+export const updateRoom = (props: CommandsSequenceInput<any>): CommandsSequenceInput<any> => {
   const { callback } = props
   try {
-    const rooms = db.rooms.filter(room => room.roomUsers.length > 0)
-    callback(rooms)
+    const rooms = db.rooms.filter(room => room.players.length > 0)
+    const mappedRooms = rooms.map(room => { 
+      return { roomId: room.id, roomUsers: room.players.map(({name, id}) => {
+        return { name, index: id }
+      }) }
+    })
+    callback(mappedRooms)
     return props
   } catch (error) {
     const errorText = error instanceof Error ? error.message : error
@@ -14,10 +21,15 @@ export const updateRoom = (props: any) => {
   }
 }
 
-export const updateWinners = (props: any) => {
+export const updateWinners = (props: CommandsSequenceInput<any>): CommandsSequenceInput<any> => {
   const { callback } = props
   try {
-    const winners = [...db.winners]
+    const winners = db.players.map(player => {
+      return { 
+        name: player.name, 
+        wins: player.wins 
+      }
+    })
     callback(winners)
     return props
   } catch (error) {
@@ -28,10 +40,12 @@ export const updateWinners = (props: any) => {
   }
 }
 
-export const createRoom = (props: any) => {
+export const createRoom = (props: CommandsSequenceInput<CreateRoomRequestData>): CommandsSequenceInput<CreateRoomRequestData> => {
   try {
+    // TODO: I need to get right player here - how do I know which one is the right one?
     const player = db.players[db.players.length - 1]
-    const newRoom = { roomId: db.rooms.length, roomUsers: [player]}
+    // TODO: I need PlayerID here.
+    const newRoom = { id: String(crypto.randomUUID()), players: [{id: 'db.players.length - 1', name: player.name}], game: null}
     db.rooms.push(newRoom)
     return props
   } catch (error) {
@@ -41,3 +55,55 @@ export const createRoom = (props: any) => {
   }
 }
 
+export const addUserToRoom = (props: CommandsSequenceInput<AddUserToRoomRequestData>): CommandsSequenceInput<AddUserToRoomRequestData> => {
+  const { data } = props
+  const { indexRoom } = data
+  try {
+    const room = db.rooms.find(room => room.id === indexRoom)
+    const player = db.players[db.players.length - 1]
+    if(room) {
+      room.players.push({ 
+        name: player.name,
+        // TODO I need to get right player here - how do I know which one is the right one? I need it's ID
+        id: 'string'
+      })
+    }
+    return props
+  } catch (error) {
+    const errorText = error instanceof Error ? error.message : error
+    console.error(errorText)
+    return props
+  }
+}
+
+export const createGame = (props: CommandsSequenceInput<string>): CommandsSequenceInput<string> => {
+  const { callback } = props
+  try {
+    // console.log('players:', players)
+    const game = { 
+      id: crypto.randomUUID(), 
+      ships: []
+    }
+    db.rooms[db.rooms.length - 1].game = game
+    // TODO: I need to get right player here - how do I know which one is the right one? I need it's ID
+    callback({idGame: game.id, idPlayer: ''})
+    return props
+  } catch (error) {
+    const errorText = error instanceof Error ? error.message : error
+    // console.log('game created error')
+    console.error(errorText)
+    return props
+  }
+}
+
+// export const addShips = (props: CommandsSequenceInput) => {}
+
+// export const startGame = (props: CommandsSequenceInput) => {}
+
+// export const turn = (props: CommandsSequenceInput) => {}
+
+// export const attack = (props: CommandsSequenceInput) => {}
+
+// export const randomAttack= (props: CommandsSequenceInput) => {}
+
+// export const finish = (props: CommandsSequenceInput) => {}
--- backend/src/service/player.service.ts
+++ backend/src/service/player.service.ts
@@ -1,22 +1,28 @@
+import crypto from 'node:crypto'
 import db from '../db/db'
+import { CommandsSequenceInput, RegisterPlayerRequestData } from '../types'
+import { getErrorMessage } from '../utils'
 
-export const registerPlayer = (props: any) => {
-  let index
+export const registerPlayer = (props: CommandsSequenceInput<RegisterPlayerRequestData>): CommandsSequenceInput<RegisterPlayerRequestData> => {
+  let index = ''
   const { data: player, callback } = props
   try {
     if (!player.name || !player.password) {
       throw new Error('Name and password are required')
     }
-    index = db.players.map(player => player.name).indexOf(player.name)
-    if(index !== -1) {
-      throw new Error('Player already exists')
+    const existingPlayer = db.players.find(playerData => playerData.name === player.name)
+    if(existingPlayer) {
+      const isPasswordCorrect = existingPlayer.password === player.password
+      callback({ name: existingPlayer.name, index: existingPlayer.id, error: !isPasswordCorrect, errorText: isPasswordCorrect ? '' : 'Name or password is incorrect'})
+      return props
     }
-    callback({ name: player?.name, index, error: false, errorText: '' })
+    const id = crypto.randomUUID()
+    db.players.push({...player, id, wins: 0})
+    callback({ name: player?.name, index: id, error: false, errorText: '' })
     return props
   } catch (error) {
-    const errorText = error instanceof Error ? error.message : error
-    console.error(errorText)
-    callback({ name: player?.name, index, error: true, errorText})
+    const errorMessage = getErrorMessage(error)
+    callback({ name: player?.name, index, error: true, errorText: errorMessage})
     return props
   }
 }
